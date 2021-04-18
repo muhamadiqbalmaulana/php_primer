342c29b3c55f1767463f5619f431ec385b4b0820
muhamadiqbalmaulana <iqbalslardar@gmail.com>
2021-04-18 20:55:27 +0700

    added link to heroku

--- index.php
+++ index.php
@@ -25,4 +25,5 @@
 <button type="button" class="btn btn-dark">CLICK ME!</button>
 <button type="button" class="btn btn-primary">CLICK ME!</button>
 <button type="button" class="btn btn-success">CLICK ME!</button>
+<a href="https://www.heroku.com" class="btn btn-danger" target="blank">Heroku.com</a>
 <?php require 'includes/footer.php'?>
